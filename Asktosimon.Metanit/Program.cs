24590231de1032cc6fe1fe4100bf9e5c64424c64
Andrey Simanovich <asktosimon@yandex.ru>
2022-01-13 16:03:10 +0300

    metanit tutor

--- Asktosimon.Metanit/Program.cs
+++ Asktosimon.Metanit/Program.cs
@@ -6,28 +6,31 @@ int[] myArray = { 1, 10, 4, 23, 223, 77, 43, 2, 4 };
 #region Bublesort
 int[] BubleSort(int[] arr)
 {
+    int[] result = new int[arr.Length - 1];
+
     for (int i = 0; i < arr.Length - 1; i++)
     {
         for (int j = 0; j < arr.Length - 1 - i; j++)
         {
-            int temp;
-
             var current = arr[j];
             var next = arr[j + 1];
 
-            if (current > next)
+            /*if (current > next)
             {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
+                result[j] = arr[j + 1];
 
-                arr[j + 1] = temp;
-            }
+                result[j + 1] = arr[j];
+            }*/
+            result[j] = arr[j];
         }
     }
 
-    return arr;
+    return result;
 }
 
-var resultBubleSort = BubleSort(myArray);
-Console.WriteLine(string.Join(", ", resultBubleSort));
+Console.WriteLine(string.Join(", ", BubleSort(myArray)));
+#endregion
+
+#region Shakersort
+
 #endregion
---
+++ teory/Program.cs
@@ -0,0 +1,40 @@
+﻿void PrintPerson(int age, string name, string company = "LG", string st = "test")
+{
+    Console.WriteLine($"{age}, {name}, {company}, {st}");
+}
+
+PrintPerson(age: 33, name: "Andrey", st: "XLEX");
+
+var lan = Languages.ru;
+
+switch(lan)
+{
+    case Languages.ru:
+        SayHelloRu();
+        break;
+    case Languages.en:
+        SayHello();
+        break;
+}
+
+void SayHello()
+{
+    PrinMessage("Hello World!");
+}
+
+
+void SayHelloRu()
+{
+    PrinMessage("Привет, мир!");
+}
+
+void PrinMessage(string message)
+{
+    Console.WriteLine(message);
+}
+
+enum Languages
+{
+    ru,
+    en
+}

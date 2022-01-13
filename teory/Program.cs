06f15532967350306a1e480397e3e743e9204b71
Andrey Simanovich <asktosimon@yandex.ru>
2022-01-13 16:33:26 +0300

    progress

--- teory/Program.cs
+++ teory/Program.cs
@@ -1,4 +1,38 @@
-﻿void PrintPerson(int age, string name, string company = "LG", string st = "test")
+﻿#region PARAMS
+void Hay(int num, params string[] names)
+{
+    foreach (string name in names)
+    {
+        Console.WriteLine(name);
+    }
+};
+
+Hay(1, "GEORG", "LAM");
+#endregion
+
+#region REF IN OUT
+void Increment(ref int num)
+{
+    num++;
+}
+
+int num = 6;
+Increment(ref num);
+Console.WriteLine(num);
+
+void Sum(int a, int b, out int sum)
+{
+    sum = a + b;
+}
+
+
+Sum(1, 3, out int result);
+
+Console.WriteLine(result);
+#endregion
+
+#region методы
+void PrintPerson(int age, string name, string company = "LG", string st = "test")
 {
     Console.WriteLine($"{age}, {name}, {company}, {st}");
 }
@@ -7,7 +41,7 @@ PrintPerson(age: 33, name: "Andrey", st: "XLEX");
 
 var lan = Languages.ru;
 
-switch(lan)
+switch (lan)
 {
     case Languages.ru:
         SayHelloRu();
@@ -38,3 +72,4 @@ enum Languages
     ru,
     en
 }
+#endregion

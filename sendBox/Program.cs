4082943ee7ee2f68d77786b12dbfb3cd852a5760
Andrey Simanovich <asktosimon@yandex.ru>
2022-01-22 11:03:20 +0300

    WIP

--- sendBox/Program.cs
+++ sendBox/Program.cs
@@ -1,1 +1,21 @@
-﻿var result = "1" + "2";
+﻿var arr = new int[1] { 0 };
+GetString(arr);
+
+Console.WriteLine(arr[0]);
+
+
+void GetString(in int[] arr)
+{
+    arr[0] = 3;
+}
+
+enum DyasOfWeek
+{
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+};

e0250e5fe7e1c6e81f3e27f711958f3ca1a88448
Andrey Simanovich <asktosimon@yandex.ru>
2022-01-27 17:33:37 +0300

    WIP

--- ServerAPI/Program.cs
+++ ServerAPI/Program.cs
@@ -1,37 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
-/*RequestDelegate res = async (context) => { context.Response.Redirect("/"); };
-RequestDelegate ress = async (context) => { await context.Request.Form["name"]; };*/
-
-/*RequestDelegate res = async (context) =>
-{
-    if (context.Request.HasJsonContentType())
-    {
-        var person = await context.Request.ReadFromJsonAsync<Person>();
-        if (person != null)
-        {
-            Console.WriteLine("person");
-        }
-    }
-};*/
-
-RequestDelegate res = async (context) =>
-{
-    Console.WriteLine("RUN");
-};
-
-async Task GetData(HttpContext context, RequestDelegate next)
-{
-    Console.WriteLine("USE after");
-    await next.Invoke(context);
-    Console.WriteLine("USE before");
-}
-
-app.Use(GetData);
-
-app.Run(res);
 
 app.Run();
-
-class Person { }
--- sendBox/Program.cs
+++ sendBox/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿var a = new Man(12);
+
+
+class Person
+{
+    protected int ID;
+
+    public Person (int ID)
+    {
+        this.ID = ID;
+        this.ID++;
+    }
+}
+
+class Man : Person
+{
+
+    public Man(int ID) : base (ID)
+    {
+        this.ID++;
+        Console.WriteLine (this.ID);
+    }
+}

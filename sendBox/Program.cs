9cdaff5714da27db9b2c40e810d61cee603aacb6
Andrey Simanovich <asktosimon@yandex.ru>
2022-01-28 15:28:00 +0300

    WIP

--- sendBox/Program.cs
+++ sendBox/Program.cs
@@ -1,16 +1,31 @@
-﻿var a = new Counter { Value = 1 };
-var b = new Counter { Value = 2 };
-
-var c = a + b;
-
-Console.WriteLine(c.Value);
-
-class Counter
+﻿class A
 {
-    public int Value { get; set; }
-
-    public static Counter operator + (Counter a, Counter b)
+    public virtual void Say()
     {
-        return new Counter { Value = a.Value + b.Value };
+        Console.WriteLine("A");
+    }
+}
+
+class B : A
+{
+    public override void Say()
+    {
+        Console.WriteLine("B");
+    }
+}
+
+class C : A
+{
+    public new void Say()
+    {
+        Console.WriteLine('C');
+    }
+}
+
+class D : B
+{
+    public override void Say()
+    {
+        Console.WriteLine("D");
     }
 }

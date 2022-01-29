48cb9370c1df905d2237dabc80f648f80a3cb258
Andrey Simanovich <asktosimon@yandex.ru>
2022-01-29 13:57:56 +0300

    WIP SWAP

--- sendBox/Program.cs
+++ sendBox/Program.cs
@@ -1,18 +1,16 @@
-﻿abstract class ComputerPart
+﻿var x = 1;
+var y = 2;
+
+Console.WriteLine($"{x}, {y}");
+Obj.Swap(ref x, ref y);
+Console.WriteLine($"{x}, {y}");
+class Obj
 {
-    public abstract void Work();
+    public static void Swap(ref int num1, ref int num2)
+    {
+        var temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
 }
 
-class MotherBoard: ComputerPart {
-    public override void Work() { }
-}
-
-class Processor: ComputerPart
-{
-    public override void Work() { }
-}
-
-class GraphicCard: ComputerPart 
-{
-    public override void Work() { }
-}

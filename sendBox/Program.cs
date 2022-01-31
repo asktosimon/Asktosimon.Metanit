be4387322e0a9290c64eda68110e0ef1a29d6797
Andrey Simanovich <asktosimon@yandex.ru>
2022-01-31 10:24:54 +0300

    WIP

--- sendBox/Program.cs
+++ sendBox/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System.Collections;
+
+string[] people = new string[] { "TOM" };
+
+IEnumerator enumerator = people.GetEnumerator();
+
+while(enumerator.MoveNext())
+{
+    Console.WriteLine(enumerator.Current);
+}
+
+enumerator.Reset();

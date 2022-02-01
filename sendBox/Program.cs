23476c432047030521132265b3abce2d24a994b8
Andrey Simanovich <asktosimon@yandex.ru>
2022-02-01 20:25:05 +0300

    WIP

--- sendBox/Program.cs
+++ sendBox/Program.cs
@@ -1,5 +1,7 @@
 ﻿string firstName = "Andrey";
 string secondName = "Simanovich";
+double price = 2.7;
 
-string fullName = string.Concat(firstName, secondName);
+string fullName = string.Format("firstName: {0}, secondName: {1}, price: {2:C2}", firstName, secondName, price);
 
+Console.WriteLine(fullName);

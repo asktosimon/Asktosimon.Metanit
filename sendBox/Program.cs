402783c9699095bdd81a2acc58f5946a903ef189
Andrey Simanovich <asktosimon@yandex.ru>
2022-02-06 12:15:41 +0300

    WIP

--- sendBox/Program.cs
+++ sendBox/Program.cs
@@ -1,15 +1,1 @@
-﻿await DoSomethingAsync();
-
-async Task DoSomethingAsync()
-{
-    Console.WriteLine("start do something");
-    await Task.Run(() => DoElseSomethingAsync());
-    Console.WriteLine("end do something");
-}
-
-async void DoElseSomethingAsync()
-{
-    Console.WriteLine("start doing something else");
-    await Task.Delay(4000);
-    Console.WriteLine("end doing something else");
-}
+﻿
